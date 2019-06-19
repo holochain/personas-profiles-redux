fd04eb823f0a11a84062bb8427efab1ccc60a120
Philip Beadle <philip.beadle@live.com.au>
2019-06-19 12:02:22 +1000

    Updated to 0.0.20

--- ui-src/src/store.ts
+++ ui-src/src/store.ts
@@ -3,7 +3,7 @@ import { holochainMiddleware } from '@holochain/hc-redux-middleware'
 import { connect } from '@holochain/hc-web-client'
 import holoVault from './hApps/holo-vault/reducer'
 let rootReducer = combineReducers({ holoVault: holoVault })
-const middleware: Array<any> = [holochainMiddleware(connect(`ws://localhost:3400`))]
+const middleware: Array<any> = [holochainMiddleware(connect())]
 
 // @ts-ignore
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
