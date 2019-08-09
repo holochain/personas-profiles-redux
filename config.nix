a428bf4f5761cde26b51d802f93355f1e3255b83
Philip Beadle <philip.beadle@live.com.au>
2019-08-09 13:08:39 +1000

    Ready for release

--- config.nix
+++ config.nix
@@ -62,7 +62,7 @@ echo "All finished!!!"
   # the commit hash that the release process should target
   # this will always be behind what ends up being deployed
   # the release process needs to add some commits for changelog etc.
-  commit = "d197dcc56d1269aefc10627dee6ef52fb8ed19c1";
+  commit = "799231f421ae63b7a705ba5b20efdde36ca641e6";
 
   # the semver for prev and current releases
   # the previous version will be scanned/bumped by release scripts
