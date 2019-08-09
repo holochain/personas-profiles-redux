799231f421ae63b7a705ba5b20efdde36ca641e6
Philip Beadle <philip.beadle@live.com.au>
2019-08-09 13:04:43 +1000

    Bump version to 0.1.7

--- config.nix
+++ config.nix
@@ -68,7 +68,7 @@ echo "All finished!!!"
   # the previous version will be scanned/bumped by release scripts
   # the current version is what the release scripts bump *to*
   version = {
-   current = "0.1.6";
+   current = "0.1.8";
    previous = "0.1.7";
   };
 
